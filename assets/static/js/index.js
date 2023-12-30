03db8775d7be6bcd22f074f251968dcae06a7265
Rafael-Benicio <jerobenicio@gmail.com>
2023-12-30 18:05:42 -0300

    build: adiciona dependencia 'eslint'

--- assets/static/js/index.js
+++ assets/static/js/index.js
@@ -1,55 +1,54 @@
-const VIDEO_PATH='videos/Eps/'
-const MOVIE_PATH='videos/Filmes/'
-const VIDEO_TAG_ID='video_view'
-const VIDEO_NAME_TAG_ID='video_card_name'
+const VIDEO_PATH='videos/Eps/';
+// const MOVIE_PATH='videos/Filmes/'
+const VIDEO_TAG_ID='video_view';
+const VIDEO_NAME_TAG_ID='video_card_name';
 
 async function getEpisodesList(){
-     let response = await fetch('http://192.168.3.30:3000/Eps');
-     let data = await response.json();
-     let listOfEpisodes=data.listEpisode[0]
-     return listOfEpisodes
+	const response = await fetch('http://192.168.3.30:3000/Eps');
+	const data = await response.json();
+	return data.listEpisode[0];
 }
 
 function generateEpisodeList(episodeList=[''],elementId='',elementIn='',path=''){
-     let get_list_ul=document.getElementById(elementId)
+	let get_list_ul=document.getElementById(elementId);
 
-     for(let index=0;index<episodeList.length;index++){
-          let elementTag=document.createElement(elementIn)
-          elementTag.innerHTML=episodeList[index]
-          elementTag.setAttribute("onclick", `setVideoPath('${path}' , '${episodeList[index]}');`);
-          elementTag.setAttribute("title", episodeList[index]);
-          get_list_ul.appendChild(elementTag)
-     }
-     setVideoPath(VIDEO_PATH,episodeList[0])
+	for(let index=0;index<episodeList.length;index++){
+		let elementTag=document.createElement(elementIn);
+		elementTag.innerHTML=episodeList[index];
+		elementTag.setAttribute('onclick', `setVideoPath('${path}' , '${episodeList[index]}');`);
+		elementTag.setAttribute('title', episodeList[index]);
+		get_list_ul.appendChild(elementTag);
+	}
+	setVideoPath(VIDEO_PATH,episodeList[0]);
 }
 
 function setVideoName(video=''){
-     const videoType=video.split('.')[0]
-     let get_video_source=document.querySelector(`#${VIDEO_NAME_TAG_ID}>p`)
-     get_video_source.innerHTML=videoType
+	const videoType=video.split('.')[0];
+	let get_video_source=document.querySelector(`#${VIDEO_NAME_TAG_ID}>p`);
+	get_video_source.innerHTML=videoType;
 }
 
 function setVideoPath(path='',video=''){
-     const newPath=path+video
-     const videoType=video.split('.')
-     let get_video_source=document.getElementById(VIDEO_TAG_ID)
-     get_video_source.setAttribute('src',newPath)
-     get_video_source.setAttribute('type',`video/${videoType[1]}`)
-     setVideoName(video)
+	const newPath=path+video;
+	const videoType=video.split('.');
+	let get_video_source=document.getElementById(VIDEO_TAG_ID);
+	get_video_source.setAttribute('src',newPath);
+	get_video_source.setAttribute('type',`video/${videoType[1]}`);
+	setVideoName(video);
 }
 
+// eslint-disable-next-line no-unused-vars
 function showVideoNameAndHide(){
-     let get_video_source=document.querySelector(`#${VIDEO_NAME_TAG_ID}`)
-     if (get_video_source.style.display === "none") {
-          get_video_source.style.display = "block";
-          setTimeout(() => {
-               get_video_source.style.display = "none";
-          }, "2000");
-     } else {
-          get_video_source.style.display = "none";
-     }
-
+	let get_video_source=document.querySelector(`#${VIDEO_NAME_TAG_ID}`);
+	if (get_video_source.style.display === 'none') {
+		get_video_source.style.display = 'block';
+		setTimeout(() => {
+			get_video_source.style.display = 'none';
+		}, '2000');
+	} else {
+		get_video_source.style.display = 'none';
+	}
 }
 
-getEpisodesList().then(promiseEpsList => generateEpisodeList(promiseEpsList,'list_videos','li',VIDEO_PATH))
+getEpisodesList().then(promiseEpsList => generateEpisodeList(promiseEpsList,'list_videos','li',VIDEO_PATH));
 
--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-app.use(express.static('assets'));
 const port = 3000;
 
+app.use(express.static('assets'));
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'pages/index.html'));
+	res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
-app.get("/Eps", (req, res) => {
-  fs.readdir('./assets/videos/Eps/', (err, files) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    res.status(200).json({listEpisode:Array(files)});
-    // console.log(Array(files));
-  });
-})
+app.get('/Eps', (req, res) => {
+	fs.readdir('./assets/videos/Eps/', (err, files) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		res.status(200).json({listEpisode:Array(files)});
+	});
+});
 
 app.listen(port,'0.0.0.0');
 console.log('Server started at http://localhost:' + port);
