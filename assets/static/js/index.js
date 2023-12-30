6dbc9f2d43f85322b720b806637807a3f002b1e8
Rafael-Benicio <jerobenicio@gmail.com>
2023-12-29 22:59:12 -0300

    refactor(static/js/index.js): reescreve nome de variaveis e funções

--- assets/static/js/index.js
+++ assets/static/js/index.js
@@ -1,19 +1,19 @@
-async function createList(){
+async function getEpisodesList(){
      let response = await fetch('http://localhost:3000/Eps');
      let data = await response.json();
-     let d=data.listEpisode[0]
-     cList(d)
+     let listOfEpisodes=data.listEpisode[0]
+     return listOfEpisodes
 }
 
-function cList(x){
-     let get_list_ul=document.getElementById('list_videos')
-     
-     for(let i=0;i<x.length;i++){
-          let li_ele=document.createElement('li')
-          li_ele.innerHTML=x[i]
+function generateEpisodeList(epList,elementId,elementIn){
+     let get_list_ul=document.getElementById(elementId)
+
+     for(let index=0;index<epList.length;index++){
+          let li_ele=document.createElement(elementIn)
+          li_ele.innerHTML=epList[index]
           get_list_ul.appendChild(li_ele)
      }
 }
 
-createList()
+getEpisodesList().then(promiseEpsList => generateEpisodeList(promiseEpsList,'list_videos','li'))
 
