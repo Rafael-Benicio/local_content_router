6308063e167b5dae21902f9bc4525cfa54f1d03a
Rafael-Benicio <jerobenicio@gmail.com>
2023-12-30 01:41:12 -0300

    feat: troca video em reprodução conforme escolha do usuario

--- assets/static/js/index.js
+++ assets/static/js/index.js
@@ -1,3 +1,7 @@
+const VIDEO_PATH='videos/Eps/'
+const MOVIE_PATH='videos/Filmes/'
+const VIDEO_TAG_ID='video_view'
+
 async function getEpisodesList(){
      let response = await fetch('http://192.168.3.30:3000/Eps');
      let data = await response.json();
@@ -5,15 +9,26 @@ async function getEpisodesList(){
      return listOfEpisodes
 }
 
-function generateEpisodeList(epList,elementId,elementIn){
+function generateEpisodeList(episodeList=[''],elementId='',elementIn='',path=''){
      let get_list_ul=document.getElementById(elementId)
 
-     for(let index=0;index<epList.length;index++){
-          let li_ele=document.createElement(elementIn)
-          li_ele.innerHTML=epList[index]
-          get_list_ul.appendChild(li_ele)
+     for(let index=0;index<episodeList.length;index++){
+          let elementTag=document.createElement(elementIn)
+          elementTag.innerHTML=episodeList[index]
+          elementTag.setAttribute("onclick", `setVideoPath('${path}' , '${episodeList[index]}');`);
+          elementTag.setAttribute("title", episodeList[index]);
+          get_list_ul.appendChild(elementTag)
      }
+     setVideoPath(VIDEO_PATH,episodeList[0])
 }
 
-getEpisodesList().then(promiseEpsList => generateEpisodeList(promiseEpsList,'list_videos','li'))
+function setVideoPath(path='',video=''){
+     const newPath=path+video
+     const videoType=video.split('.')
+     let get_video_source=document.getElementById(VIDEO_TAG_ID)
+     get_video_source.setAttribute('src',newPath)
+     get_video_source.setAttribute('type',`video/${videoType[1]}`)
+}
+
+getEpisodesList().then(promiseEpsList => generateEpisodeList(promiseEpsList,'list_videos','li',VIDEO_PATH))
 
